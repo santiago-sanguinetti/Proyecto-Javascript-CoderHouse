52eaf0ead1d96b614dc6ff038a21fb7a7a3f9cd9
Santiago Sanguinetti <sanguinettisantiago@gmail.com>
2023-02-15 16:44:51 -0300

    Segunda entrega

--- js/script.js
+++ js/script.js
@@ -1,52 +1,96 @@
 // ----------------------- Funciones auxiliares
-function calcularCuotas(total, cantidadDeCuotas){
-    return total/cantidadDeCuotas;
+function calcularCuotas(total, cantidadDeCuotas) {
+    if (cantidadDeCuotas != 0) {
+        return (total / cantidadDeCuotas).toFixed(2);
+    }
+    return "";
 }
+
+function listaDeJuegosPrompt() {
+    let textoParaPrompt =
+        "Ingrese un número para agregar al carrito o 0 para salir.";
+
+    let numJuego = 1;
+    arrJuegos.forEach((juego) => {
+        textoParaPrompt += `\n${numJuego} - ${juego.nombre} (US$${juego.precio})`;
+        numJuego++;
+    });
+
+    return textoParaPrompt;
+}
+
+class Juego {
+    constructor(nombre, plataforma, precio) {
+        this.nombre = nombre;
+        this.plataforma = plataforma;
+        this.precio = precio;
+    }
+}
+
+let theWitcher3PC = new Juego("The Witcher 3", "PC", 39.99);
+let redDeadRedemption2PC = new Juego("Red Dead Redemption 2", "PC", 59.99);
+let hadesPC = new Juego("Hades", "PC", 24.99);
+
+let arrJuegos = [theWitcher3PC, redDeadRedemption2PC, hadesPC];
 
 let plataforma = prompt("Por favor ingrese su plataforma (PC, PS, XBOX):");
 
-while (plataforma != "PC" && plataforma != "PS" && plataforma != "XBOX"){
+while (plataforma != "PC" && plataforma != "PS" && plataforma != "XBOX") {
     alert("La plataforma ingresada no se encuentra");
     plataforma = prompt("Por favor ingrese su plataforma (PC, PS, XBOX):");
-    console.log(plataforma);
 }
 
 let montoTotal = 0;
 
 if (plataforma == "PC") {
-    alert("Bienvenido a la plataforma PC, a continuación ingrese el número del juego para agregarlo a su carrito.")
-    
+    alert(
+        "Bienvenido a la plataforma PC, a continuación ingrese el número del juego para agregarlo a su carrito."
+    );
+
     let juegoElegido = -1;
-    while (juegoElegido != 0){
-        juegoElegido = parseInt(prompt("Ingrese un número para agregar al carrito o 0 para salir. \n1 - The Witcher 3 (US$39.99)\n2 - Red Dead Redemption 2 (US$59.99)\n3 - Hades (US$24.99)"))
+    let juegosEnCarrito = [];
+    while (juegoElegido != 0) {
+        juegoElegido = parseInt(prompt(listaDeJuegosPrompt()));
 
         switch (juegoElegido) {
             case 1:
-                montoTotal = montoTotal + 39.99;
-            break;
+                juegosEnCarrito.push(theWitcher3PC);
+                break;
             case 2:
-                montoTotal = montoTotal + 59.99;
-            break;
+                juegosEnCarrito.push(redDeadRedemption2PC);
+                break;
             case 3:
-                montoTotal = montoTotal + 24.99;
-            break;
+                juegosEnCarrito.push(hadesPC);
+                break;
         }
     }
 
+    juegosEnCarrito.forEach((juego) => {
+        montoTotal += juego.precio;
+    });
+
     alert(`El total es de ${montoTotal}.`);
     let cuotas = prompt("¿En cuantas cuotas quiere realizar el pago?");
-    while (isNaN(cuotas) || parseInt(cuotas) <= 0){
-        alert ("La cantidad de cuotas debe ser un número entero mayor que 0.");
+    while (isNaN(cuotas) || parseInt(cuotas) <= 0) {
+        alert("La cantidad de cuotas debe ser un número entero mayor que 0.");
         cuotas = prompt("¿En cuantas cuotas quiere realizar el pago?");
     }
 
-    alert (`Debe pagar ${cuotas} cuotas de ${calcularCuotas(montoTotal, cuotas)}US$`);
-    
+    alert(
+        `Debe pagar ${cuotas} cuotas de US$${calcularCuotas(
+            montoTotal,
+            cuotas
+        )}`
+    );
 } else if (plataforma == "PS") {
-    alert("La plataforma para PS no se encuentra implementada actualmente. Lamentamos los inconvenientes.")
+    alert(
+        "La plataforma para PS no se encuentra implementada actualmente. Lamentamos los inconvenientes."
+    );
 } else if (plataforma == "XBOX") {
-    alert("La plataforma para XBOX no se encuentra implementada actualmente. Lamentamos los inconvenientes.")
+    alert(
+        "La plataforma para XBOX no se encuentra implementada actualmente. Lamentamos los inconvenientes."
+    );
 } else {
     //A este else no debería entrar nunca, ya se valida en el while.
-    alert("La plataforma ingresada es incorrecta.")
+    alert("La plataforma ingresada es incorrecta.");
 }
